85a4a31f34663e88a981cf0dfe007d9a6c3c1a5c
吳宗融 <bc781208@hotmail.com>
2020-08-14 16:23:16 +0800

    預告片放大縮小

--- cent/main.php
+++ cent/main.php
@@ -6,10 +6,12 @@
           <div class="mainBimg">
 <?php
   $rows=all('pop',['sh'=>1],' order by ord');
-  foreach ($rows as $r) {
+foreach ($rows as $r) {
 ?>
 <div class="popimg"> 
   <img src="./img/<?=$r['name'];?>" class="popBimg">
+  <input type="hidden" name="type" value="<?=$r['type'];?>">
+  <div class="popBdiv"><?=$r['text'];?></div>
 </div>
 
 <?php
@@ -41,3 +43,34 @@
       </div>
     </div>
   </div>
+<script>
+let ii=0;
+$(".popimg").hide();
+$(".popimg").eq(0).show();
+let autochg=setInterval(() => {
+  chgpage()
+}, 3000);
+function chgpage() {
+  let e=$(".popimg").eq(ii).find("input[name='type']")[0].value*1
+  let max=$(".popimg").length
+  let kk=ii+1
+  if(ii+1==max)kk=0
+  // console.log(e,ii,kk)
+  switch (e) {
+    case 1:
+    $(".popimg").eq(ii).fadeOut(2000);
+    $(".popimg").eq(ii+1).fadeIn(2000);
+      break;
+    case 2:
+    $(".popimg").eq(ii).slideUp(1000,()=>{
+      $(".popimg").eq(ii+1).slideDown(1000);
+    });
+      break;
+    case 3:
+    $(".popimg").eq(ii).hide(1000,()=>{
+      $(".popimg").eq(ii+1).show(1000);
+    });
+  }
+  ii=kk;
+}
+</script>
